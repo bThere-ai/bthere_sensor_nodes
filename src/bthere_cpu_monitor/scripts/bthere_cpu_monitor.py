6c8968be7ca2162341a42ed6b532154109e3b9c4
Theo Marshall <theo@seelye.net>
2020-07-07 20:57:04 -0700

    misc. improvements based on code review feeback such as: merging of all data into one topic comments and code simplification for clarity removal of redundant code

--- src/bthere_cpu_monitor/scripts/bthere_cpu_monitor.py
+++ src/bthere_cpu_monitor/scripts/bthere_cpu_monitor.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 
-from rospy import init_node, loginfo, logerr, ROSInterruptException, Publisher, Rate, is_shutdown, get_param
-from std_msgs.msg import Int8, Float32
+from rospy import init_node, loginfo, logerr, ROSInterruptException, Publisher, Rate, is_shutdown, get_param, Time
 from CPUData.msg import CPUData
 from os import listdir
 from glob import glob
 
-#expands a list to make sure there is space up to a given index.
-def ensure_space_in_list(list, index, fill_with):
-    if(index >= len(list)):
-        num_needed = index - len(list) + 1
-        for i in range(0, num_needed):
-            list.append(None)
-
-
-#returns: a list where item 0 is package temp, and items after that are sequential core temps.
+#returns: a tuple of type (float, float[]) where the the first element is CPU package (overall) temperature 
+#         in degrees C, and the second element is a list of per-core CPU temperatures (also deg. C).
+#         Will return None if an error is encountered.
+#
+#note: this function is dependent on the numbering of the files in the directory for the cpu in 
+# /sys/class/hwmon/ being consistent, which they may not be.
+#      The structure seems to be that in /sys/class/hwmon there will be a directory (among others) containing
+#      a file "name" containing simply "coretemp", with contents such as "temp1_input, temp1_crit, temp1_label,
+#      temp2_input, temp2_crit, temp2_label", etc. Each of the tempX files corresponds to a temperature sensor
+#      as labeled by tempX_label as "Package id Y" for the CPU package, "Core Y" for a specific core, etc.
 def get_cpu_temps():
     try:
         hwmons = listdir("/sys/class/hwmon")
-        ret = []
+        package_temp = None
+        core_temps = []
         cpu_hwmon_path = ""
         for hwmon in hwmons:
             name_file = open("/sys/class/hwmon/" + hwmon + "/name")
@@ -28,7 +29,9 @@ def get_cpu_temps():
                 name_file.close()
                 break
             name_file.close()
-        for path in glob(cpu_hwmon_path + "/temp*_label"):
+        labels = glob(cpu_hwmon_path + "/temp*_label")
+        labels.sort()
+        for path in labels:
             label_file = open(path)
             label = label_file.read().strip()
             temperature_file_path = path[:29] + "_input" #should end up with something like "/sys/class/hwmon/hwmonX/tempY_input" for this label
@@ -36,60 +39,87 @@ def get_cpu_temps():
             temperature = float(temperature_file.read().strip()) / 1000
             temperature_file.close()
             if("Package" in label):
-                ret[0] = temperature
-            else: #this is for a core
-                core = int(label[5:])
-                ensure_space_in_list(ret, core + 1, None)
-                ret[core + 1] = temperature
+                # ret[0] = temperature
+                package_temp = temperature
+            else: #this is (probably) for a core.
+                core_temps.append(temperature)
             label_file.close()
-        return ret
+        return (package_temp, core_temps)
     except: #there is a lot of stuff that can break in the above block...
         logerr("unable to get CPU temperature data")
         return None
 
+#parameters: last_cpu_times: a 2d list of strings, as produced by get_load_data(). used to calculate a
+#           change since the last call.
+#
+#returns: a tuple of(float, float[], str[][]) where the first float is overall CPU load, the second element
+#        is a list of CPU loads for each CPU core, and the final element is the data most recently received 
+#        from calling get_load_data() to be used for the next call of this function.
 def get_cpu_load(last_cpu_times):
-    proc_stat = open("/proc/stat", "r")
-    times_since_startup = proc_stat.readline().strip().split()[1:]
-    proc_stat.close()
-    difference = []
-    for i in range(0, len(times_since_startup)):
-        difference.append(int(times_since_startup[i]) - int(last_cpu_times[i]))
-    idle = float(difference[3]) / float(sum(difference))
-    return int(round((1 - idle) * 100))
+    new_cpu_times = get_load_data()
+    overall = None
+    per_core = []
+    for line_index in range(0, len(last_cpu_times)):
+        difference = []
+        for i in range(0, len(last_cpu_times[0])):
+            difference.append(int(new_cpu_times[line_index][i]) - int(last_cpu_times[line_index][i]))
+        idle = float(difference[3]) / float(sum(difference)) # %/100 of time since startup spent in an "idle" state
+        load = 1 - idle # %/100 of time since startup spent not idle, i.e. 
+        if(line_index == 0):
+            overall = round(load, 3)
+        else:
+            per_core.append(round(load, 3)) #these values are probably not _super_ precise, so rounding to the nearest 10th seems about right?
+    return (overall, per_core, new_cpu_times)
 
-def get_init_cpu_load():
+#returns a 2d list of strings where each element is a list times spent in various stats since startup, measured in USER_HZ (usually 10ms).
+# The first element is for the system overall, subsequent elements are for specific cores, in order.
+# This data comes directly from /proc/stat. more info: https://man7.org/linux/man-pages/man5/proc.5.html
+def get_load_data():
     proc_stat = open("/proc/stat", "r")
-    times_since_startup = proc_stat.readline().strip().split()[1:]
+    ret = []
+    #times_since_startup = proc_stat.readline().strip().split()[1:]
+    for line in proc_stat:
+        line_split = line.strip().split()
+        if(not ("cpu" in line_split[0])): #we have gone past the CPU lines
+            break
+        else:
+            ret.append(line_split[1:]) #everything but the label since we know [0] is overall and after that is per core by index
     proc_stat.close()
-    return times_since_startup
+    return ret
 
 def cpu_monitor():
     init_node("bthere_cpu_monitor", anonymous=False)
-    load_pub = Publisher("/bthere/cpu_load", Int8, queue_size=5)
+    pub = Publisher("/bthere/cpu_data", CPUData, queue_size=10)
     
+    #update period should to be somewhat small since the cpu load data is average since you last checked,
+    #a slower update rate will be less accurate for bursty loads and may introduce more lag than expected
+    #if a load is added later in the time between updates for example.
     update_period = get_param('~update_period', 1.0)
     rate = Rate(1/float(update_period))
 
+    #since the temperature-getting seems likely to be failure prone, try it once to check.
     able_to_get_temps = False
-    temps_pub = None
     if(get_cpu_temps() != None):
         able_to_get_temps = True
-        temps_pub = Publisher("/bthere/cpu_temps", CPUData, queue_size = 10)
     
     last_cpu_times = []
     while not is_shutdown():
+        data = CPUData()
         if(able_to_get_temps):
-            cpu_data_list = get_cpu_temps()
-            temps = CPUData()
-            temps.package_temp = cpu_data_list[0]
-            temps.core_temps = cpu_data_list[1:]
-            temps_pub.publish(temps)
-        if(len(last_cpu_times) == 0):
-            last_cpu_times = get_init_cpu_load()
+            package_temp, core_temps = get_cpu_temps()
+            data.package_temp = package_temp
+            data.core_temps = core_temps
         else:
-            cpu_load = get_cpu_load(last_cpu_times)
-            load_pub.publish(cpu_load)
-        
+            #data is unavailable so just make it NaN
+            data.package_temp = float("NaN")
+            data.core_temps = [float("NaN")]
+        if(len(last_cpu_times) == 0): #if this hasn't been initialized, we just won't publish this info yet.
+            last_cpu_times = get_load_data()
+        else:
+            overall_load, per_cores, last_cpu_times = get_cpu_load(last_cpu_times)
+            data.overall_cpu_load = cpu_load
+        data.timestamp = Time.now()
+        pub.publish(data)
         rate.sleep()
 
 
